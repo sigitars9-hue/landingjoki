c08c8b5a6a814daf7051baf57a2014dd569b4eb2
unknown <sigitaria2@gmail.com>
2025-10-22 18:36:38 +0700

    fix: vite path untuk build di Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({ plugins: [react()] })
+export default defineConfig({
+  plugins: [react()],
+  base: './', // penting agar path relatif saat deploy ke Vercel
+})
+
